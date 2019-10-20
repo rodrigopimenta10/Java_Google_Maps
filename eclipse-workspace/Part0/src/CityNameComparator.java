be9d56a0b2eb9edc19df633a613ba520279dd5e9
Rodrigo Pimenta <rodrigopimenta10@gmail.com>
2019-10-20 13:31:33 -0400

    Adding  eclipse we workspace used for maps project

---
+++ eclipse-workspace/Part0/src/CityNameComparator.java
@@ -0,0 +1,11 @@
+import java.util.Comparator;
+
+public class CityNameComparator implements Comparator<String>
+{
+            public int compare (String s1, String s2) {
+                        
+                     return 0;
+
+            }
+
+}
